2091ec993e878df1894e2162917bab4cc27a368d
Daniel <danielfisch9@gmail.com>
2022-11-03 23:11:34 -0400

    added screenshot functionality

--- main.js
+++ main.js
@@ -6,16 +6,30 @@ const URL = "https://scp-wiki.wikidot.com/scp-series";
 const scrape = function () {
   (async () => {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 25,
-      args: ["--start-maximized"],
+      //args: [""],
       defaultViewport: null,
     }); //launch a browser with puppeteer
     const page = await browser.newPage(); //create a new tab
-    
+    var j = 0;
     await page.goto(URL); // go to https://scp-wiki.wikidot.com/scp-series
-   
-  })();
+   var links =await page.$$eval('h1 + ul > li > a', anchors => [].map.call(anchors, a => a.href));
+   for(var i = 1; i <  links.length - 1;i++){
+      await page.goto(links[i]);
+      
+      await page.screenshot({
+        path: ('screenshots/scp'+ j++ + '.jpeg'),
+        fullPage : 'true'
+      });
+      sleep(1000/6);
+      await page.goBack();
+   }
+    // await page.goto(link);
+   await page.close();
+   await browser.close();
+   }
+  )();
 
   function sleep(ms) {
     // function to sleep so that page elements can properly load before being interacted with.
